16dc00e4fd72e39a8dc50b5afcc4afd2ebf1b82a
TemitopeAde22 <temitope224468@gmail.com>
2025-05-23 05:22:53 +0100

    update

--- dev.tfvars
+++ dev.tfvars
@@ -1,3 +1,3 @@
 region = "eu-west-2"
 email_endpoint = "adebunmi33@gmail.com"
-lambda_function_name = "cost-optimizer"
+lambda_function_name = "underutilized-resource-detector"
--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "cost_optimizer_prod" {
-  source              = "./Modules/Cost-Alerting"
+  source              = "./Modules/Under-Utilized-Resources"
   region              = var.region
   email_endpoint      = var.email_endpoint
   lambda_function_name = var.lambda_function_name
