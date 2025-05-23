b8ce1fefa8c1df97705c3b9ae8d0ddcfd5dc2a6e
TemitopeAde22 <temitope224468@gmail.com>
2025-05-23 04:56:06 +0100

    update

--- Modules/Cost-Alerting/main.tf
+++ Modules/Cost-Alerting/main.tf
@@ -46,7 +46,7 @@ resource "aws_lambda_function" "cost_optimizer" {
   filename         = data.archive_file.lambda_zip.output_path
   function_name    = var.lambda_function_name
   role             = aws_iam_role.lambda_exec_role.arn
-  handler          = "lambda_function.lambda_handler"
+  handler          = "lambda.lambda_handler"
   runtime          = "python3.9"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   timeout          = 30
